70f7649394253c38e3189980bb29f897107ca537
Lukas Oberholzer <lukaober17@computercamp.local>
2017-08-10 16:25:52 +0200

    Added Audio Manager

---
+++ src/w3r3w0lf/AudioManager.java
@@ -0,0 +1,18 @@
+package w3r3w0lf;
+
+import java.io.File;
+import java.net.Socket;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+
+public class AudioManager {
+	public static void PlayAudio(String sourceFile)
+	{
+		String audioFile= "\\src\\resources\\" + sourceFile.toString();
+		Media hit = new Media(new File(audioFile).toURI().toString());
+		MediaPlayer mediaPlayer = new MediaPlayer(hit);	
+		mediaPlayer.play();
+	}
+	
+}
